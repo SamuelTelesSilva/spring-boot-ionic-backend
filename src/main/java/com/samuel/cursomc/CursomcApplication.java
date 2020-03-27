0524dd57f912d449dc7898f0707eeb26e3802d6e
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-03-27 18:50:27 -0300

    Paginação com parametros opcionais na requisição e Spring Data

--- src/main/java/com/samuel/cursomc/CursomcApplication.java
+++ src/main/java/com/samuel/cursomc/CursomcApplication.java
@@ -62,6 +62,10 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama");
+		Categoria cat4 = new Categoria(null, "Mesa");
+		Categoria cat5 = new Categoria(null, "Decoração");
+		Categoria cat6 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -74,7 +78,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais"); 

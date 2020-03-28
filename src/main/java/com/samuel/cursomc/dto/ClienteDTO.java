19b5bf9ab99befcd23da25ff77cd0903439dd77c
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-03-28 12:52:42 -0300

    Validando customizada email não repetido na atualização de cliente

--- src/main/java/com/samuel/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/samuel/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.samuel.cursomc.domain.Cliente;
+import com.samuel.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 

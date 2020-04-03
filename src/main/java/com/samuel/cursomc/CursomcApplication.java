5135020c46a4a5ae6a65703fe11346f9a69e6503
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-04-03 02:17:10 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/samuel/cursomc/CursomcApplication.java
+++ src/main/java/com/samuel/cursomc/CursomcApplication.java
@@ -1,14 +1,18 @@
 package com.samuel.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.samuel.cursomc.service.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		
@@ -18,7 +22,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 	
-		
+		s3Service.uploadFile("C:\\Users\\Samuel\\Desktop\\Programação\\Python\\Godot\\Game\\cenarioo.jpg");
 		}
 
 }

e47ae52e9c7aa4222a23e41113f5c5344aaef4d7
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-04-03 02:48:05 -0300

    Enviando imagem via endpoint´

--- src/main/java/com/samuel/cursomc/CursomcApplication.java
+++ src/main/java/com/samuel/cursomc/CursomcApplication.java
@@ -1,18 +1,14 @@
 package com.samuel.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.samuel.cursomc.service.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	
-	@Autowired
-	private S3Service s3Service;
+	
 	
 	public static void main(String[] args) {
 		
@@ -22,7 +18,7 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 	
-		s3Service.uploadFile("C:\\Users\\Samuel\\Desktop\\Programação\\Python\\Godot\\Game\\cenarioo.jpg");
+		
 		}
 
 }
